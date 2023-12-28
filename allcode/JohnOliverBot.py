906717247b06f6ae1666e8b44fd7e106219c46c1
Aditya Rajeev <umailadi@gmail.com>
2023-12-27 22:51:18 -0800

    Fixed bot code  Please enter the commit message for your changes. Lines starting

--- allcode/JohnOliverBot.py
+++ allcode/JohnOliverBot.py
@@ -20,20 +20,30 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
     print(client.user.id)
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        'leave.'
-    )
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send('leave.')
+
+nikhil_names = {'blu', 'kane', 'nikhil', 690756558698840157}
+akash_names = {'akash', 393251455685099521}
+adi_names = {'adi', 'steel', 'steelcrawler', 464402798235222027}
+anish_names = {'swishy', 'anish', 573361953649590274}
+aneesh_names = {'rams', 'bloom', 'aneesh', 577675221352579095}
+
 
 @client.event
 async def on_message(message):
-    if message.author.id in (541319380919910400, 724681050395115520):
-        await message.add_reaction("\U0001F913") 
+    if '!history' == message.content.lower():
+        messages = [message1 async for message1 in message.channel.history()]
+        for message1 in messages:
+            await message1.channel.send(message1.content)
 
-    if  'im not' in message.content.lower():
-        await message.channel.send("yes u are")
+    # if message.author.id in (541319380919910400, 724681050395115520):
+    #     await message.add_reaction("\U0001F913") 
+
+    # if  'im not' in message.content.lower():
+    #     await message.channel.send("yes u are")
     
-    if message.author.id == 393251455685099521:
-        await message.add_reaction("\U0001F5FA")
+    # if message.author.id == 393251455685099521:
+    #     await message.add_reaction("\U0001F5FA")
